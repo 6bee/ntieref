99bc5c2510a1551f5a67b5e4dfabbc70e43716cb
ChristofSenn <christof.senn@gmail.com>
2014-08-08 01:14:26 +0200

    added sample application for table inheritance

---
+++ samples/TableInheritanceDemo/TableInheritance/TableInheritance.Client.Domain.Test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NTier.Client.Domain;
+using NTier.Common.Domain.Model;
+using TableInheritance.Client.Domain;
+using TableInheritance.Common.Domain.Model.TableInheritanceDemoDB;
+
+namespace TableInheritance.Client.Domain.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ctx = new TableInheritanceDemoDBDataContext();
+            var customer = new Customer
+            {
+                FirstName = "Peter",
+                LastName = "Pan",
+                CustomerStatus = 1,
+            };
+            ctx.Add(customer);
+            ctx.SaveChanges();
+        }
+    }
+}
