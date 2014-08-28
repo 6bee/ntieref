c3938a1c406df0822b8302152fe14201e5d00d60
ChristofSenn <christof.senn@gmail.com>
2014-08-28 22:42:06 +0200

    fix: allow access to navigation property members when translating SortDescription into SortExpression

--- src/N-Tier Entity Framework/NTier.Client.Domain/SortDescriptionExtensions.cs
+++ src/N-Tier Entity Framework/NTier.Client.Domain/SortDescriptionExtensions.cs
@@ -1,9 +1,10 @@
 ﻿// Copyright (c) Trivadis. All rights reserved. See license.txt in the project root for license information.
 
+using NTier.Common.Domain.Model;
+using Remote.Linq.Expressions;
 using System;
 using System.ComponentModel;
 using System.Reflection;
-using NTier.Common.Domain.Model;
 using RLinq = Remote.Linq.Expressions;
 
 namespace NTier.Client.Domain
@@ -12,12 +13,23 @@ namespace NTier.Client.Domain
     {
         public static RLinq.SortExpression ToExpression<T>(this SortDescription sortDescription) where T : Entity
         {
+            if (ReferenceEquals(null, sortDescription)) throw new ArgumentNullException("sortDescription");
             var propertyName = sortDescription.PropertyName;
-            var entityType = typeof(T);
-            var propertyInfo = entityType.GetProperty(propertyName, BindingFlags.Instance | BindingFlags.Public);
-            var parameterExpression = RLinq.Expression.Parameter("i", typeof(T));
-            var propertyExpression = RLinq.Expression.MakeMemberAccess(parameterExpression, propertyInfo);
-            var lambdaExpression = RLinq.Expression.Lambda(propertyExpression, parameterExpression);
+            if (string.IsNullOrEmpty(propertyName)) throw new Exception("property name must not be null or empty");
+
+            var type = typeof(T);
+            var propertyNames = propertyName.Split('.');
+            var parameterExpression = RLinq.Expression.Parameter("i", type);
+
+            Expression expression = parameterExpression;
+            foreach (var property in propertyNames)
+            {
+                var propertyInfo = type.GetProperty(property, BindingFlags.Instance | BindingFlags.Public);
+                expression = RLinq.Expression.MakeMemberAccess(expression, propertyInfo);
+                type = propertyInfo.PropertyType;
+            }
+
+            var lambdaExpression = RLinq.Expression.Lambda(expression, parameterExpression);
             var sortDirection = sortDescription.Direction.Translate();
             var sortExpression = RLinq.Expression.Sort(lambdaExpression, sortDirection);
             return sortExpression;
@@ -33,7 +45,6 @@ namespace NTier.Client.Domain
                 default:
                     throw new Exception(string.Format("Unmapped sort direction: {0}", direction));
             }
-        }
- 
+        } 
     }
 }
