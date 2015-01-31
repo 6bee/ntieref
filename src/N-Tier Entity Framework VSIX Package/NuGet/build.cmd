242677ba632d5a388eb5448273e5db7823c9722f
ChristofSenn <christof.senn@gmail.com>
2015-01-31 23:20:55 +0100

    1.5.1

--- src/N-Tier Entity Framework VSIX Package/NuGet/build.cmd
+++ src/N-Tier Entity Framework VSIX Package/NuGet/build.cmd
@@ -1,3 +1,3 @@
 @ECHO OFF
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild "%~dp0\NuGet.proj" /v:minimal /maxcpucount /nodeReuse:false /p:Version=1.5.1 %*
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild "%~dp0\NuGet.proj" /v:minimal /maxcpucount /nodeReuse:false /p:Version=1.5.2 %*
 pause
