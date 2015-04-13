8cb0b2c4e7dbe5d2bd86de2a7451c2e17f97eacb
ChristofSenn <christof.senn@gmail.com>
2015-04-13 23:34:59 +0200

    release 1.6 updated to remote.linq 4.1.0

--- src/N-Tier Entity Framework VSIX Package/NuGet/build.cmd
+++ src/N-Tier Entity Framework VSIX Package/NuGet/build.cmd
@@ -1,3 +1,3 @@
 @ECHO OFF
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild "%~dp0\NuGet.proj" /v:minimal /maxcpucount /nodeReuse:false /p:Version=1.6.0-beta003 %*
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild "%~dp0\NuGet.proj" /v:minimal /maxcpucount /nodeReuse:false /p:Version=1.6.0 %*
 pause
