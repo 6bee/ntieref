d6d01a9847fff0dbdae24bdca351b220b6fbb202
ChristofSenn <christof.senn@gmail.com>
2015-06-01 22:14:31 +0200

    release 1.7

--- src/N-Tier Entity Framework VSIX Package/NuGet/build.cmd
+++ src/N-Tier Entity Framework VSIX Package/NuGet/build.cmd
@@ -1,3 +1,3 @@
 @ECHO OFF
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild "%~dp0\NuGet.proj" /v:minimal /maxcpucount /nodeReuse:false /p:Version=1.7.0-beta004 %*
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild "%~dp0\NuGet.proj" /v:minimal /maxcpucount /nodeReuse:false /p:Version=1.7.0 %*
 pause
--- src/N-Tier Entity Framework VSIX Package/build vsix 2012.cmd
+++ src/N-Tier Entity Framework VSIX Package/build vsix 2012.cmd
@@ -1,3 +1,3 @@
 @ECHO OFF
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild "%~dp0\N-Tier Entity Framework VSIX Package.v11.sln" /target:Rebuild /verbosity:minimal /maxcpucount /nodeReuse:false /property:Configuration=Release;VsixVersion=1.7;NugetPackageVersion=1.7.0-beta004;VisualStudioVersion=12.0 %*
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild "%~dp0\N-Tier Entity Framework VSIX Package.v11.sln" /target:Rebuild /verbosity:minimal /maxcpucount /nodeReuse:false /property:Configuration=Release;VsixVersion=1.7;NugetPackageVersion=1.7.0;VisualStudioVersion=12.0 %*
 pause
