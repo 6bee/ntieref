e58e605d977efc07471ffa57f6f85c22f9bf55b3
ChristofSenn <christof.senn@gmail.com>
2015-09-30 23:16:43 +0200

    1.8.1

--- src/N-Tier Entity Framework VSIX Package/NuGet/build.cmd
+++ src/N-Tier Entity Framework VSIX Package/NuGet/build.cmd
@@ -1,3 +1,3 @@
 @ECHO OFF
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild "%~dp0\NuGet.proj" /v:minimal /maxcpucount /nodeReuse:false /p:Version=1.8.0;VisualStudioVersion=12.0 %*
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild "%~dp0\NuGet.proj" /v:minimal /maxcpucount /nodeReuse:false /p:Version=1.8.1;VisualStudioVersion=12.0 %*
 pause
---
+++ test/UnitTests/UnitTests/UnitTests/ClientDataContext/SimpleParentChildDataContext/When_reverting_changes.cs
@@ -0,0 +1,60 @@
+﻿namespace UnitTests.ClientDataContext.SimpleParentChildDataContext
+{
+    using Test.Client.Domain;
+    using Test.Common.Domain.Model.SimpleParentChild;
+    using Test.Common.Domain.Service.Contracts;
+
+    using Xunit;
+    using Xunit.Should;
+
+    public class When_reverting_changes
+    {
+        private readonly SimpleParentChildDataContext context;
+
+        private readonly Parent parent;
+
+        public When_reverting_changes()
+        {
+            context = new SimpleParentChildDataContext(() => default(ISimpleParentChildDataService));
+
+            parent = new Parent { Id = 1 };
+
+            context.ParentSet.Attach(parent);
+
+            context.AcceptChanges();
+
+            var child1 = new Child { Id = 2 };
+            var child2 = new Child { Id = 3 };
+
+            context.ChildSet.Add(child1);
+            context.ChildSet.Add(child2);
+
+            parent.Children.Add(child1);
+            parent.Children.Add(child2);
+        }
+
+        [Fact]
+        public void Context_should_initially_have_changes()
+        {
+            context.HasChanges.ShouldBeTrue();
+            
+            context.ParentSet.Count.ShouldBe(1);
+            context.ChildSet.Count.ShouldBe(2);
+
+            parent.Children.Count.ShouldBe(2);
+        }
+
+        [Fact]
+        public void Context_should_be_unchanged_after_reverting()
+        {
+            context.RevertChanges();
+
+            context.HasChanges.ShouldBeFalse();
+
+            parent.Children.ShouldBeEmpty();
+
+            context.ParentSet.Count.ShouldBe(1);
+            context.ChildSet.Count.ShouldBe(0);
+        }
+    }
+}
