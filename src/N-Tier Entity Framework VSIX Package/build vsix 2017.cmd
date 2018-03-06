79fd1378e6b4d1856e2f7ae64bd5e357f51bc104
Senn, Christof <Christof.Senn@bis.org>
2018-03-06 15:51:11 +0100

    nuget restore prior build

--- src/N-Tier Entity Framework VSIX Package/build vsix 2017.cmd
+++ src/N-Tier Entity Framework VSIX Package/build vsix 2017.cmd
@@ -1,8 +1,12 @@
 @ECHO OFF
+ECHO.
+ECHO RESTORE NUGET PACKAGES
+"%~dp0..\..\tools\NuGet\NuGet.exe" restore "%~dp0..\N-Tier Entity Framework\N-Tier Entity Framework.sln"
+ECHO.
+ECHO BUILD AND PACKAGE
 SETLOCAL
 PATH=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin;%programfiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin;%ProgramFiles(x86)%\MSBuild\14.0\Bin;%SystemRoot%\Microsoft.NET\Framework\v4.0.30319;%PATH%
-
 MSBuild.exe "%~dp0\N-Tier Entity Framework VSIX Package.v15.sln" /target:Rebuild /verbosity:minimal /maxcpucount /nodeReuse:false /property:Configuration=Release;VsixVersion=1.9.1;NugetPackageVersion=1.9.1;VisualStudioVersion=15.0 %*
-
 ENDLOCAL
+ECHO.
 PAUSE
