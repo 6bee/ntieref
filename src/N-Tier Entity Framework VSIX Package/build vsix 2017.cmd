3050c57a15e75d4e4cea9355c430589ee8744e4c
6bee <christof.senn@gmail.com>
2018-03-07 07:01:40 +0100

    revert nuget package id to 1.9.0

--- src/N-Tier Entity Framework VSIX Package/build vsix 2017.cmd
+++ src/N-Tier Entity Framework VSIX Package/build vsix 2017.cmd
@@ -6,7 +6,7 @@ ECHO.
 ECHO BUILD AND PACKAGE
 SETLOCAL
 PATH=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin;%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin;%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin;%ProgramFiles(x86)%\MSBuild\14.0\Bin;%SystemRoot%\Microsoft.NET\Framework\v4.0.30319;%PATH%
-MSBuild.exe "%~dp0\N-Tier Entity Framework VSIX Package.v15.sln" /target:Rebuild /verbosity:minimal /maxcpucount /nodeReuse:false /property:Configuration=Release;VsixVersion=1.9;NugetPackageVersion=1.9.1;VisualStudioVersion=15.0 %*
+MSBuild.exe "%~dp0\N-Tier Entity Framework VSIX Package.v15.sln" /target:Rebuild /verbosity:minimal /maxcpucount /nodeReuse:false /property:Configuration=Release;VsixVersion=1.9;NugetPackageVersion=1.9.0;VisualStudioVersion=15.0 %*
 ENDLOCAL
 ECHO.
 PAUSE
