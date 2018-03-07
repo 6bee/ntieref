4a261b4519b6ec2468446c505c0579b9c409dd28
6bee <christof.senn@gmail.com>
2018-03-07 18:31:08 +0100

    re-activated Silverlight package build for VS2010 - VS2015

--- src/N-Tier Entity Framework VSIX Package/NuGet/build.cmd
+++ src/N-Tier Entity Framework VSIX Package/NuGet/build.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
 SETLOCAL
-PATH=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin;%programfiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin;%ProgramFiles(x86)%\MSBuild\14.0\Bin;%SystemRoot%\Microsoft.NET\Framework\v4.0.30319;%PATH%
+PATH=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin;%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin;%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin;%ProgramFiles(x86)%\MSBuild\14.0\Bin;%SystemRoot%\Microsoft.NET\Framework\v4.0.30319;%PATH%
 
 MSBuild.exe "%~dp0\NuGet.proj" /v:minimal /maxcpucount /nodeReuse:false /p:Version=1.9.1;VisualStudioVersion=15.0 %*
 
